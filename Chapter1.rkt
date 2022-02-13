fbacec27e969a0d5af68b68c9ee759224bb0eb64
Ashish Kuamr Sharma <ashish6194@users.noreply.github.com>
2022-02-13 13:24:49 +0530

    Create Chapter1.rkt

---
+++ Chapter1.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Chapter1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Add comment
+
+(+ 3 4) ;7
+(+ 3 (* 2 3)) ; 9
+
+(sqr 3) ; 9
+
+(sqrt 16) ; 4
